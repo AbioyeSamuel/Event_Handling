ef914a979e7ded5f8dd034a710685082e6023d06
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-27 10:28:52 +0100

    mouseOver mouseOut implemented on button via useState hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
-
+  const [isMouseOver, setMouseOver] = useState(false);
   function handleClicked() {
     setHeadingText("Submitted");
   }
-
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClicked}>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClicked}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
